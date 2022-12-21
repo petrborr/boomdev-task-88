b0f7c0490cb12cc4e740f26ee229424634b40471
petrborr <petrborr@gmail.com>
2022-12-21 22:01:53 +0200

    add: spring animation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import "./scss/app.scss";
 import Application from "./js/Application";
+import anime from "animejs";
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
+  let article = document.querySelector('.article');
+  
+  article.addEventListener("click", ()=>{
+    app.emit("click")
+  })
+  
   // Used to access the app instance by the automated tests
   window.__JS_APP = app;
 });
